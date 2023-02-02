3daf27d3d410b66adbfbc08bb138b132bfbbb622
Dimas Mahendra <mahendradimas21@gmail.com>
2023-02-02 16:22:17 +0700

    currency converter

--- helpers/currencyConverter.js
+++ helpers/currencyConverter.js
@@ -4,7 +4,8 @@ export default function currencyConverter(num) {
     return '-'
   }
 
-  return num.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', minimumFractionDigits: 0})
+  return num.toLocaleString({minimumFractionDigits: 0})
+  // return num.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', minimumFractionDigits: 0})
 
   // Deprecating this since old browser doesn't support Intl yet.
   // return new Intl.NumberFormat('id-ID', {style: 'currency', currency: 'IDR', minimumFractionDigits: 0}).format(num)
